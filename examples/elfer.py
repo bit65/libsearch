962092dd6bc8b4718b0a427978987d85e852098f
Knightingale <kg@cyberknights.io>
2018-11-19 12:15:37 +0200

    Refactoring ELF parsing

--- examples/elfer.py
+++ examples/elfer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
-from libsearch import apk_downloader
-from libsearch import extract_zip
-from libsearch import parse_so
+from libsearch.processing.so import parse_so
 
 parse_so("", "cache/lib/armeabi-v7a/libunity.so")
-
-# extract_zip("sample.apk")
--- libsearch/processing/so.py
+++ libsearch/processing/so.py
@@ -1,8 +1,17 @@
+from elftools.elf.elffile import ELFFile
+from cxxfilt import demangle
+
 def parse_so(file_name, orig_name):
 
     if orig_name.startswith("lib/"):
         _, system, name =  orig_name.split('/')
-        
+
         print "LIB", system, name
-    else: 
+    else:
         print "SO", orig_name
+
+    with open(orig_name, "rb") as f:
+        e = ELFFile(f)
+        for s in e.iter_sections():
+            if s['sh_type'] == 'SHT_STRTAB':
+                print [demangle(x) for x in s.data().split("\x00")]
