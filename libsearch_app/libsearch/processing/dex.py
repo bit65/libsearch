2708e09fa3710e1ed2b588ca817431a70e2dacd6
irotem <rotemb@gmail.com>
2018-12-05 15:04:41 +0200

    Working with androgaurd dex because of memory problems

--- libsearch_app/libsearch/processing/dex.py
+++ libsearch_app/libsearch/processing/dex.py
@@ -1,20 +1,22 @@
 from libsearch.processing.base import ParserBase
 from libsearch.enrichment.module_mapper import ModuleMapper
-import pydexinfo
+# import pydexinfo
+from androguard.core.bytecodes import dvm
 import subprocess
 import re
 import os
+import gc
 # from ..dbmodels import *
 
 class DEXParser(ParserBase):
 
     def __init__(self, filename):
-        self.module_mapper = ModuleMapper()
+        # self.module_mapper = ModuleMapper()
         ParserBase.__init__(self, filename)
 
     parsetype = "application/octet-stream"
     ext = "dex"
-
+    
     def _parse_modules(self, s):
         module_info = []
         # print s
@@ -28,10 +30,10 @@ class DEXParser(ParserBase):
         missing_module_libs = []
         for m in modules:
             module_info.append(self.createData("MODULES", m))
-            _libs = self.module_mapper.search(m)
-            libs += _libs
-            if len(_libs) == 0:
-                missing_module_libs.append(m)            
+            # _libs = self.module_mapper.search(m)
+            # libs += _libs
+            # if len(_libs) == 0:
+            #     missing_module_libs.append(m)            
 
         for lib in list(set(libs)):
             module_info.append(self.createData("LIB", lib))
@@ -47,12 +49,26 @@ class DEXParser(ParserBase):
 
         return module_info
 
+    def readFromDEX(self, f):
+        d = dvm.DalvikVMFormat(f.read())
+        classes = []
+        for c in d.get_classes():
+            classes.append('.'.join(c.get_name().split('/')[:-1])[1:])
+
+        del d
+        gc.collect()
+        return list(set(classes))
+
+
     def _parse(self, f):
         print "PARSING DEX"
-        s = pydexinfo.parse(f, True)
-        information = []
+                
+        classes = self.readFromDEX(f)
 
-        information += self._parse_modules(s)
+        # s = pydexinfo.parse(f, True)
+        information = [self.createData("MODULES", i) for i in classes]
+
+        # information += self._parse_modules(s)
 
         print "DONE DEX"
         return information
