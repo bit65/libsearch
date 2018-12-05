9a18191e94142c65a885a36ca2606ac1f2588f4b
irotem <rotemb@gmail.com>
2018-12-06 00:15:07 +0200

    Indexing fixes

--- libsearch_app/examples/zip_test.py
+++ libsearch_app/examples/zip_test.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os 
+import os
 from libsearch.processing.searchparser import Parser
 from libsearch.storage.indexer import Indexer
 
+import datetime
+
 host = os.getenv('ES_HOST', "localhost")
 aws_options = {
-    "hosts":[{'host': host, 'port': 443}],
-    "use_ssl":True,
-    "verify_certs":True
+    "hosts": [{'host': host, 'port': 443}],
+    "use_ssl": True,
+    "verify_certs": True
 }
-
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/../../cache"
 
 files = os.listdir(dir_path)
-files = files[:10]
 for f in files:
-    parser = Parser.instance().get_parser(dir_path + os.sep + f)
-    if parser != None:
-        index_data = parser.parse()
-        Indexer.instance(aws_options).save(index_data)
+    if f.endswith(".apk"):
+        if os.path.isfile(dir_path + os.sep + f + '.indexed'):
+            continue
+
+        try:
+            parser = Parser.instance().get_parser(dir_path + os.sep + f)
+            if parser != None:
+                index_data = parser.parse()
+                Indexer.instance(aws_options).save(index_data)
+                with open(dir_path + os.sep + f + '.indexed', 'w') as cached:
+                    cached.write(str(datetime.date.today()))
+        except Exception as e:
+            pass
+
 print "Done"
-
-
--- libsearch_app/libsearch/storage/indexer.py
+++ libsearch_app/libsearch/storage/indexer.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 import hashlib
 import datetime
 
+
 def xstr(s):
     if s is None:
         return ''
-    return str(s)
+    return unicode(s)
+
 
 class Indexer:
     _instance = None
@@ -24,24 +28,22 @@ class Indexer:
         self.index_name = index_name
         self.create_index(index_name)
 
-
     def save(self, data):
         if isinstance(data, list):
             docs = [{
-                    "_index": self.index_name,
-                    "_type": "_doc",
-                    "_id": xstr(doc['TYPE']) + xstr(doc['ASSET']) + xstr(doc['VALUE']),
-                    "_source": doc
-                    } for doc in data]
+                "_index": self.index_name,
+                "_type": "_doc",
+                "_id": xstr(doc['TYPE']) + xstr(doc['ASSET']) + xstr(doc['VALUE']),
+                "_source": doc
+            } for doc in data]
             before = datetime.datetime.now()
             helpers.bulk(self.es, docs)
-            after = datetime.datetime.now()            
+            after = datetime.datetime.now()
             print "Bulk Indexed %s docs in %s" % (len(docs), str(after - before))
-            
+
             # self.save(doc)
         else:
-            if ('TYPE' in data and 'ASSET' in data and 'VALUE' in data):
-
+            if 'TYPE' in data and 'ASSET' in data and 'VALUE' in data:
                 id_hash = hashlib.sha256(xstr(data['TYPE']) + xstr(data['ASSET']) + xstr(data['VALUE']))
                 self.es.index(index=self.index_name, id=id_hash.hexdigest(), doc_type='_doc', body=data)
 
@@ -52,20 +54,20 @@ class Indexer:
             "settings": {
                 "number_of_shards": 1,
                 "number_of_replicas": 0,
-                "analysis":{
-                    "analyzer":{
-                        "splitter_analyzer":{ 
-                            "type":"custom",
-                            "tokenizer":"splitter_tokenizer",
-                            "filter":[
+                "analysis": {
+                    "analyzer": {
+                        "splitter_analyzer": {
+                            "type": "custom",
+                            "tokenizer": "splitter_tokenizer",
+                            "filter": [
                                 "lowercase"
                             ]
                         }
                     },
                     "tokenizer": {
                         "splitter_tokenizer": {
-                        "type": "pattern",
-                        "pattern": "[\s\.\\/_:*-]"
+                            "type": "pattern",
+                            "pattern": "[\\s\\.\\\\/_:*-]"
                         }
                     }
                 },
@@ -87,10 +89,10 @@ class Indexer:
                             "fields": {
                                 "splitted": {
                                     "type": "text",
-                                    "analyzer":"splitter_analyzer"
+                                    "analyzer": "splitter_analyzer"
                                 }
                             }
-                            
+
                         }
                     }
                 }
